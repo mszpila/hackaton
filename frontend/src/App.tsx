c064d36e5a0169d38856d3f146a3f6a98eb9ca38
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-12-02 23:00:01 +0100

    adding header, sidebar, main-interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
+import React from 'react'
+
+//import { Col, Divider, Row } from 'antd';
 
 import './App.css';
+import Header from './Components/Header';
+import MainInterface from './Components/MainInterface';
+import SideBar from './Components/SideBar';
 
 function App() {
+
+
   return (
     <div className='App'>
-      <h1>Hello Hackathon</h1>
+      
+
+      <Header />
+      
+
+      <SideBar />
+
+      
+
+      <MainInterface />
+
     </div>
   );
 }
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+    return ( 
+        <div id="header">
+            <h2>schedule</h2>
+            <h2>Health</h2>
+        </div>
+     );
+}
+
+export default Header;
