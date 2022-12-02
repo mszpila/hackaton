219c44021733846902919f8096a38f3cc99e3ecf
Ulan Sydygaliev <60165668+Aitsuken@users.noreply.github.com>
2022-12-02 22:37:13 +0100

    added antd and some experiments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,37 @@
 
 import './App.css';
+import React from 'react';
+
+
+import { Layout } from 'antd';
+import { Button, Space } from 'antd';
 
 function App() {
   return (
-    <div className='App'>
+ 
+
+    <React.Fragment>
+      <Layout>
+                <div className='App'>
       <h1>Hello Hackathon</h1>
-    </div>
+        </div>
+      </Layout>
+    
+    
+      <Space wrap>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+
+
+      </Space>
+    
+    </React.Fragment>
+
+
+
+
+
   );
 }
 
