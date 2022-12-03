6d02647f89a0580d7ce192561d7cf8df7f158c00
bm-Yassine <83090064+bm-Yassine@users.noreply.github.com>
2022-12-03 08:29:01 +0100

    yo

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import { FC, ReactNode } from 'react';
 import Header from '../Components/Header';
-import MainHeader from './MainHeader';
+
 
 const MainLayout: FC<{ children: ReactNode }> = ({ children }) => (
   <Layout>
