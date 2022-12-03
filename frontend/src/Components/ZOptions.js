6c7e4bd9b9c0fbb97b7d8812b0b151f93f5edb3c
Ulan Sydygaliev <60165668+Aitsuken@users.noreply.github.com>
2022-12-03 06:12:42 +0100

    Added number of persons and extra options on 1st page

--- frontend/src/Components/ZOptions.js
+++ frontend/src/Components/ZOptions.js
@@ -1,18 +1,22 @@
 import './ZOptions.css';
 
-function ZOptions( {setFilter} ){
-    return ( 
-        <div id="zoptions">
-          <form>
-            <input type="checkbox" id="vegan" value="vegan"/><label for="vegan">Vegan:</label>
-            <br></br>
-            <input type="checkbox" id="gluten" value="gluten"/><label for="Gluten-free">GlutenFree</label>
-            <br/>
-            <input type="checkbox" id="lactose" value="lactose"/><label for="Lactose intolerant">Lactose</label>
-            <br/>
-          </form>
-        </div>
-     );
+function ZOptions({ setFilter }) {
+  return (
+    <div id="zoptions">
+      <form>
+        <input type="checkbox" id="VEGAN" value="VEGAN" /><label for="VEGAN">Vegan</label>
+        <br />
+        <input type="checkbox" id="VEGETARIAN" value="VEGETARIAN" /><label for="VEGETARIAN">Vegetarian</label>
+        <br />
+        <input type="checkbox" id="GLUTEN_FREE" value="GLUTEN_FREE" /><label for="GLUTEN_FREE">Gluten Free</label>
+        <br />
+        <input type="checkbox" id="DAIRY_FREE" value="DAIRY_FREE" /><label for="DAIRY_FREE">Dairy Free</label>
+        <br />
+        <input type="checkbox" id="KETOGENIC" value="KETOGENIC" /><label for="KETOGENIC">Ketogenic</label>
+        <br />
+      </form>
+    </div>
+  );
 }
 
 export default ZOptions;
--- frontend/src/Components/ZQuestion.js
+++ frontend/src/Components/ZQuestion.js
@@ -1,11 +1,19 @@
 import './ZQuestion.css';
-function ZQuestion( {setNumberOfCooking, numberOfCooking} ){
+function ZQuestion( 
+    {setNumberOfCooking, numberOfCooking} ,
+    //additional params
+    {setNumberOfPeople, numberOfPeople}
+    ){
 
     return(
         <div id="zquestions">
             <p>How many meals you want to cook this week  bi?</p>
             <input type="number" id="lname" name="lname" onChange={(event) => setNumberOfCooking(event.target.value)} value={numberOfCooking}/>
-            <br></br>
+            <br/>
+            <p>Number of People? </p>
+            <br/>
+            <input type="number" id="lpeople" name="lpeople" onChange={(event) => setNumberOfPeople(event.target.value)} value={numberOfPeople}/>
+            
         </div>
         // 1 => 14 same
         // 2 => 2 mals acrros week how? 
